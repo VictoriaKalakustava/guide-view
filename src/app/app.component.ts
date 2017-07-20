189ae631c4f055f2c45c508b957d04aefb65a733
VictoriaKalakustava <VictoriaKalakustava@gmail.com>
2017-07-20 16:32:39 +0300

    i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import {TranslateService} from "@ngx-translate/core";
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  constructor(private translate: TranslateService){
+    this.initLanguage();
+  }
+
+  private initLanguage() {
+    this.translate.addLangs(["en", "ru"]);
+    this.translate.setDefaultLang('en');
+
+    let browserLang = this.translate.getBrowserLang();
+    this.changeLanguage(browserLang.match(/en|ru/) ? browserLang : 'en')
+  }
+
+  changeLanguage(language: string) {
+    this.translate.use(language);
+  }
 }
--- src/app/component/HeaderComponent/HeaderComponent.ts
+++ src/app/component/HeaderComponent/HeaderComponent.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'header',
+  selector: 'header-form',
   templateUrl: './HeaderComponent.html',
-  StyleUrls: ['./HeaderComponent.css']
+  styleUrls: ['./HeaderComponent.css'],
 })
 export class HeaderComponent {
+
 }
