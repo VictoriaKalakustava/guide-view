031fed2f75cef5aa3205bd774ad084ce02bc44f8
VictoriaKalakustava <VictoriaKalakustava@gmail.com>
2017-07-23 01:12:36 +0300

    Added login validation base

--- src/app/component/LogInComponent/login.component.ts
+++ src/app/component/LogInComponent/login.component.ts
@@ -1,6 +1,8 @@
 import {animate, Component, state, style, transition, trigger} from "@angular/core";
 import {User} from "../../entity/user";
 import {UserService} from "../../service/user.service";
+
+declare var $: any;
 @Component({
   selector: 'login-form',
   templateUrl: './login.component.html',
@@ -8,23 +10,65 @@ import {UserService} from "../../service/user.service";
 
 })
 export class LoginComponent {
-  constructor(private userService : UserService) {
+  isLoginExist: boolean;
+  constructor(private userService: UserService) {
 
   }
-  private user: User;
-  signUp(value :any) {
-    this.user = new User();
+
+  private user: User = new User();
+  formErrors = {
+    username: '',
+  };
+
+  //to do: correct value to if
+  signUp(value: any) {
+    if (true) {
+      this.sendSignUpRequest(value);
+    } else {
+    }
+    console.log('signup');
+  }
+
+  checkLogin() {
+    var login: String;
+    login = $('#username');
+    this.formErrors.username = '';
+    console.log('logincheck');
+    if(this.user.login !== '') {
+      this.loginIsExist(login);
+    } else {
+      this.formErrors.username = 'Login is required.';
+    }
+  }
+
+  loginIsExist(login: String) {
+    this.userService.isExistLogin(login).subscribe(
+      data => {
+        this.isLoginExist = data.json();
+        if(this.isLoginExist) {
+          this.formErrors.username = 'Such login is exist.';
+        } else {
+          this.isLoginExist = false;
+        }
+      }
+    )
+  }
+
+  getUserForSignUp(value: any) {
+    console.log('getUser');
     this.user.login = value.username;
     this.user.email = value.email;
     this.user.password = value.password;
     this.user.name = value.name;
     this.user.surname = value.surname;
-    this.userService.signUp(this.user).subscribe(
-      data => {console.log(data)}
-    )
-    console.log(this.user);
   }
 
-
-
+  sendSignUpRequest(value: any) {
+    this.getUserForSignUp(value);
+    this.userService.signUp(this.user).subscribe(
+      data => {
+        console.log(data)
+      }
+    )
+  }
 }
