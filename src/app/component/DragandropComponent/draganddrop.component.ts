b20d1eff7a452a5c1b7d8d5e8c8850172c33f4af
VictoriaKalakustava <VictoriaKalakustava@gmail.com>
2017-07-28 21:05:50 +0300

    Fixed styles

--- src/app/component/DragandropComponent/draganddrop.component.ts
+++ src/app/component/DragandropComponent/draganddrop.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-draganddrop-form',
   templateUrl: './draganddrop.component.html',
-  styleUrls: ['./draganddrop.component.css'],
+  styleUrls: ['./draganddrop.component.css', '../InstructionpageComponent/ImageareaComponent/imagearea.component.css'],
 })
 export class DraganddropComponent {
 }
