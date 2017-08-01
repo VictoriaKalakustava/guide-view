3a80c9162e175b355934ef0778895251a8dbef81
VictoriaKalakustava <VictoriaKalakustava@gmail.com>
2017-08-02 01:07:02 +0300

    added change language with little fix view and code style

---
+++ src/app/component/InstructionComponent/AddInstructionComponent/add.instruction.component.ts
@@ -0,0 +1,32 @@
+import {Component} from '@angular/core';
+import Step from "../../../entity/step";
+
+
+@Component({
+  selector: 'add-instruction-component',
+  templateUrl: './add.instruction.component.html',
+  styleUrls: ['./add.instruction.component.css', '../../InstructionpageComponent/IntructionpageRedComponent/instructionpagered.component.css',
+  '../instruction.component.css'],
+})
+
+export class AddInstructionComponent {
+  containers: Step[];
+
+  constructor() {
+    console.log("constructor");
+    this.containers = [];
+    this.containers.push(new Step("a", "a"));
+    this.containers.push(new Step("b", "b"));
+    this.containers.push(new Step("c", "c"));
+    this.containers.push(new Step("d", "d"));
+    this.containers.push(new Step("dasds", "a"));
+    this.containers.push(new Step("1111b", "b"));
+    this.containers.push(new Step("222c", "c"));
+    this.containers.push(new Step("333d", "d"));
+    this.containers.push(new Step("a", "a"));
+    this.containers.push(new Step("b", "b"));
+    this.containers.push(new Step("c", "c"));
+    this.containers.push(new Step("d", "d"));
+    console.log("n init " + this.containers);
+  }
+}
